85a537caf923b431617d7376f5a2cde3e62327f0
Shazia Khan <shaziya5416@gmail.com>
2022-03-06 12:26:47 +0530

    update

---
+++ block-BNaade/routes/users.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var router = express.Router();
+var User = require("../models/user");
+
+//user Routes
+//Creating new user
+router.get("/new", (req, res) => {
+  res.render("userForm.ejs");
+});
+
+//Saving data
+router.post("/", (req, res, next) => {
+  User.create(req.body, (err, createduser) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/users");
+  });
+});
+
+//fetch the user
+router.get("/", (req, res, next) => {
+  User.find({}, (err, users) => {
+    if (err) return next(err);
+    res.render("users.ejs", { users: users });
+  });
+});
+
+//fetch only one user
+router.get("/:id", (req, res, next) => {
+  var id = req.params.id;
+  User.findById(id, (err, user) => {
+    if (err) return next(err);
+    res.render("singleUser", { user });
+  });
+});
+
+//updating user form
+router.get("/:id/edit", (req, res, next) => {
+  var id = req.params.id;
+  User.findById(id, (err, user) => {
+    if (err) return next(err);
+    res.render("editUser", { user: user });
+  });
+});
+
+//update user
+router.post("/:id", (req, res, next) => {
+  var id = req.params.id;
+  User.findByIdAndUpdate(id, req.body, (err, updatedUser) => {
+    if (err) return next(err);
+    res.redirect("/users/" + id);
+  });
+});
+
+//delete user
+router.delete("/:id", (req, res, next) => {});
+
+module.exports = router;
